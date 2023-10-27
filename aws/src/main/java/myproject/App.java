e8615a3458548b087409e355ca0ba65d7493cb71
Xiaosong-zhai <115197218+xiaosong-zhai@users.noreply.github.com>
2023-10-27 16:17:18 -0700

    update rds instance

--- aws/src/main/java/myproject/App.java
+++ aws/src/main/java/myproject/App.java
@@ -9,9 +9,15 @@ import com.pulumi.aws.ec2.inputs.RouteTableRouteArgs;
 import com.pulumi.aws.ec2.inputs.SecurityGroupEgressArgs;
 import com.pulumi.aws.ec2.inputs.SecurityGroupIngressArgs;
 import com.pulumi.aws.inputs.GetAvailabilityZonesArgs;
+import com.pulumi.aws.rds.ParameterGroup;
+import com.pulumi.aws.rds.ParameterGroupArgs;
+import com.pulumi.aws.rds.SubnetGroup;
+import com.pulumi.aws.rds.SubnetGroupArgs;
+import com.pulumi.aws.rds.inputs.ParameterGroupParameterArgs;
 import com.pulumi.core.Output;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) {
@@ -159,6 +165,7 @@ public class App {
             publicSubnets.add(publicSubnet);
             privateSubnets.add(privateSubnet);
           }
+
           // create public route table association
           for (int i = 0; i < publicSubnets.size(); i++) {
             Subnet publicSubnet = publicSubnets.get(i);
@@ -245,26 +252,184 @@ public class App {
             String amiId = AMiId.get();
 
             sgId.applyValue(id -> {
-                List<String> securityGroups = Collections.singletonList(id);
-                var webappInstance = new Instance("webapp", InstanceArgs.builder()
-                        .instanceType("t2.micro")
-                        .vpcSecurityGroupIds(securityGroups)
-                        .ami(amiId)
-                        .subnetId(publicSubnets.get(0).id())
-                        .associatePublicIpAddress(true)
-                        .disableApiTermination(false)
-                        .keyName("test")
-                        .rootBlockDevice(InstanceRootBlockDeviceArgs.builder()
-                                .volumeSize(25)
-                                .volumeType("gp2")
-                                .deleteOnTermination(true)
-                                .build())
-                        .tags(Map.of("Name", "webapp"))
+                // create a database security group
+                Optional<String> dbSecurityGroupTagNameValue = config.get("dbSgTagNameValue");
+                // check config
+                if(dbSecurityGroupTagNameValue.isEmpty()) {
+                    throw new RuntimeException("dbSgTagNameValue must be configured");
+                }
+                // get config value to string
+                String dbSgTagNameValue = dbSecurityGroupTagNameValue.get();
+                // create a security group
+                var dbSecurityGroup = new SecurityGroup(dbSgTagNameValue, SecurityGroupArgs.builder()
+                        .vpcId(main.id())
+                        .description("Security group for database")
+                        .ingress(Arrays.asList(
+                                SecurityGroupIngressArgs.builder()
+                                        .description("MariaDB")
+                                        .fromPort(3306)
+                                        .toPort(3306)
+                                        .protocol("tcp")
+                                        .securityGroups(appSecurityGroup.id().applyValue(List::of))
+                                        .build()
+                        ))
                         .build());
+                var SecurityGroupRule = new SecurityGroupRule("dbSecurityGroupRule", SecurityGroupRuleArgs.builder()
+                        .type("egress")
+                        .fromPort(3306)
+                        .toPort(3306)
+                        .protocol("tcp")
+                        .securityGroupId(appSecurityGroup.id())
+                        .sourceSecurityGroupId(dbSecurityGroup.id())
+                        .build());
+
+                // create a parameter group
+                Optional<String> dbParameterGroupName = config.get("dbParameterGroupName");
+                Optional<String> dbParameterGroupFamily = config.get("dbParameterGroupFamily");
+                // check config
+                if(dbParameterGroupName.isEmpty() || dbParameterGroupFamily.isEmpty()) {
+                    throw new RuntimeException("dbParameterGroupName and dbParameterGroupFamily must be configured");
+                }
+                // get config value to string
+                String dbParameterGroupNameValue = dbParameterGroupName.get();
+                String dbParameterGroupFamilyValue = dbParameterGroupFamily.get();
+                // create a parameter group
+                var dbParameterGroup = new ParameterGroup(dbParameterGroupNameValue, ParameterGroupArgs.builder()
+                        .family(dbParameterGroupFamilyValue)
+                        .description("Parameter group for MariaDB")
+                        .parameters(Arrays.asList(
+                                new ParameterGroupParameterArgs.Builder()
+                                        .name("max_connections")
+                                        .value("100")
+                                        .applyMethod("immediate")
+                                        .build(),
+                                new ParameterGroupParameterArgs.Builder()
+                                        .name("query_cache_size")
+                                        .value("67108864")  // 64MB in bytes
+                                        .applyMethod("immediate")
+                                        .build(),
+                                new ParameterGroupParameterArgs.Builder()
+                                        .name("innodb_buffer_pool_size")
+                                        .value("134217728")  // 128MB in bytes
+                                        .applyMethod("immediate")
+                                        .build()
+
+                        ))
+                        .build());
+                // create a mariaDB instance
+                Optional<String> dbTagNameKey = config.get("dbTagNameKey");
+                Optional<String> dbTagNameValue = config.get("dbTagNameValue");
+                Optional<String> dbInstanceName = config.get("dbInstanceName");
+                Optional<String> dbInstanceClass = config.get("dbInstanceClass");
+                Optional<String> dbMasterUsername = config.get("dbMasterUsername");
+                Optional<String> dbMasterPassword = config.get("dbMasterPassword");
+                // check config
+                if(dbTagNameKey.isEmpty() || dbTagNameValue.isEmpty() || dbInstanceName.isEmpty() || dbInstanceClass.isEmpty() || dbMasterUsername.isEmpty() || dbMasterPassword.isEmpty()) {
+                    throw new RuntimeException("dbTagNameKey, dbTagNameValue, dbInstanceName, dbInstanceClass, dbMasterUsername and dbMasterPassword must be configured");
+                }
+                // get config value to string
+                String dbTagNameKeyString = dbTagNameKey.get();
+                String dbTagNameValueString = dbTagNameValue.get();
+                String dbInstanceNameString = dbInstanceName.get();
+                String dbInstanceClassString = dbInstanceClass.get();
+                String dbMasterUsernameString = dbMasterUsername.get();
+                String dbMasterPasswordString = dbMasterPassword.get();
+
+                // create a private subnet group
+                var privateSubnetIds = Output.all(privateSubnets.stream().map(Subnet::id).collect(Collectors.toList()));
+
+                var dbPrivateSubnetGroup = new SubnetGroup("db_private_subnet_group", SubnetGroupArgs.builder()
+                        .subnetIds(privateSubnetIds.applyValue(ids -> ids))
+                        .build()
+                );
+
+
+                // create a mariaDB instance
+                var dbInstance = new com.pulumi.aws.rds.Instance(dbInstanceNameString, com.pulumi.aws.rds.InstanceArgs.builder()
+                        .engine("mariadb")
+                        .engineVersion("10.4.31")
+                        .instanceClass(dbInstanceClassString)
+                        .allocatedStorage(20)
+                        .dbSubnetGroupName(dbPrivateSubnetGroup.name())
+                        .vpcSecurityGroupIds(dbSecurityGroup.id().applyValue(List::of))
+                        .parameterGroupName(dbParameterGroup.name())
+                        .username(dbMasterUsernameString)
+                        .password(dbMasterPasswordString)
+                        .skipFinalSnapshot(true)
+                        .publiclyAccessible(false)
+                        .dbName(dbInstanceNameString)
+                        .multiAz(false)
+                        .tags(Map.of(dbTagNameKeyString, dbTagNameValueString))
+                        .build());
+
+                dbInstance.address().apply(address -> {
+                    // create UserData script
+                    String userData = String.join("\n",
+                            "#!/bin/bash",
+                            "sudo groupadd csye6225",
+                            "sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225",
+                            "cat > /opt/csye6225/application-demo.yml <<EOL",
+                            "server:",
+                            "  port: 8080",
+                            "spring:",
+                            "  application:",
+                            "    name: csye6225",
+                            "  profiles:",
+                            "    active: demo",
+                            "  main:",
+                            "    allow-circular-references: true",
+                            "  datasource:",
+                            "    driver-class-name: org.mariadb.jdbc.Driver",
+                            "    url: jdbc:mariadb://"+ address +":3306/csye6225?createDatabaseIfNotExist=true",
+                            "    username: " + dbMasterUsernameString,
+                            "    password: " + dbMasterPasswordString,
+                            "  jpa:",
+                            "    hibernate:",
+                            "      ddl-auto: update",
+                            "    properties:",
+                            "      hibernate:",
+                            "        show_sql: true",
+                            "        format_sql: true",
+                            "        dialect: org.hibernate.dialect.MariaDBDialect",
+                            "    database-platform: org.hibernate.dialect.MariaDBDialect",
+                            "csv:",
+                            "  file:",
+                            "    # path: \"classpath:static/users.csv\"",
+                            "    path: \"file:/opt/csye6225/users.csv\"",
+                            "EOL",
+                            "sudo mv /opt/webapp.jar /opt/csye6225/webapp.jar",
+                            "sudo mv /opt/users.csv /opt/csye6225/users.csv",
+                            "sudo chown csye6225:csye6225 /opt/csye6225/webapp.jar",
+                            "sudo chown csye6225:csye6225 /opt/csye6225/users.csv",
+                            "sudo chown csye6225:csye6225 /opt/csye6225/application-demo.yml",
+                            "sudo systemctl enable /etc/systemd/system/csye6225.service",
+                            "sudo systemctl start csye6225.service"
+                    );
+
+
+                    List<String> securityGroups = Collections.singletonList(id);
+                    var webappInstance = new Instance("webapp", InstanceArgs.builder()
+                            .instanceType("t2.micro")
+                            .vpcSecurityGroupIds(securityGroups)
+                            .ami(amiId)
+                            .subnetId(publicSubnets.get(0).id())
+                            .associatePublicIpAddress(true)
+                            .disableApiTermination(false)
+                            .keyName("test")
+                            .userData(userData)
+                            .rootBlockDevice(InstanceRootBlockDeviceArgs.builder()
+                                    .volumeSize(25)
+                                    .volumeType("gp2")
+                                    .deleteOnTermination(true)
+                                    .build())
+                            .tags(Map.of("Name", "webapp"))
+                            .build());
+
+                    return Output.ofNullable(null);
+                });
                 return null;
             });
-
-          return null;
+            return null;
         });
 
     }
